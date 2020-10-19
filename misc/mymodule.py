3d240cd07ee6f4477d756e03612beb1b4f9d5bcc
unknown <fchen1101@outlook.com>
2020-10-19 13:40:22 +0800

    New notes the second class

---
+++ misc/mymodule.py
@@ -0,0 +1,14 @@
+
+""" Simple module file """
+
+para = 1.0
+
+
+def myfunc():
+    print('myfunc in mymodule.')
+
+
+class MyClass:
+    def __init__(self):
+        print('MyClass in mymodule.')
+
